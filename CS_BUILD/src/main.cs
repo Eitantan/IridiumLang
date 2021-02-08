04edd11908aba4c8138f85b7fce6fb24d776f819
staticvoidliam7 <63507206+staticvoidliam7@users.noreply.github.com>
2021-02-08 12:28:21 -0500

    Update main.cs

--- CS_BUILD/src/main.cs
+++ CS_BUILD/src/main.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  public static void Main () {
+    Console.WriteLine(">>");
   }
 }
